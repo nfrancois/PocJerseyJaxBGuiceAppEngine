b8455d07dd50aa4a3cc2bf25448088a32597c36c
nfrancois <nicolas.franc@gmail.com>
2011-02-20 20:09:03 +0000

    git-svn-id: http://nicolasfrancois.googlecode.com/svn/trunk/PocJerseyJaxbGuiceAppEngine@63 0c6e3020-dc5f-4b39-ec15-eab0643b8c4e

--- src/test/java/nfrancois/poc/jerseyjaxbguiceappengine/HelloResourceTest.java
+++ src/test/java/nfrancois/poc/jerseyjaxbguiceappengine/HelloResourceTest.java
@@ -8,20 +8,23 @@ import static org.fest.assertions.Assertions.*;
 import org.junit.Test;
 
 import com.google.inject.servlet.GuiceFilter;
+import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.ClientResponse.Status;
 import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.test.framework.AppDescriptor;
 import com.sun.jersey.test.framework.JerseyTest;
 import com.sun.jersey.test.framework.WebAppDescriptor;
 
 public class HelloResourceTest extends JerseyTest {
 	
-    public HelloResourceTest() {
-        super(new WebAppDescriptor.Builder()
-                .contextListenerClass(GuiceServletConfig.class)
-                .filterClass(GuiceFilter.class)
-                .servletPath("/")
-                .build());
-    }
+	@Override
+	protected AppDescriptor configure() {
+		return new WebAppDescriptor.Builder()
+							        .contextListenerClass(GuiceServletConfig.class)
+							        .filterClass(GuiceFilter.class)
+							        .servletPath("/")
+							        .build();
+	}
 	
 	
 	@Test
@@ -29,18 +32,13 @@ public class HelloResourceTest extends JerseyTest {
 		String relativeUrl = "hello";
 		String name ="Nicolas";
 		WebResource path = resource().path(relativeUrl).path(name);
-		assertThat(path.getURI().toString()).isEqualTo(getFullUrl(relativeUrl, name));
-		int status = path.getRequestBuilder().head().getStatus();
-		assertThat(path.getRequestBuilder().head().getType()).isEqualTo(MediaType.APPLICATION_XML_TYPE);
-		assertThat(status).isEqualTo(Status.OK.getStatusCode());
-		Hello response = path.get(Hello.class);
-		assertThat(response).isNotNull();
-		assertThat(response.getMessage()).isEqualTo("Hello");
-		assertThat(response.getName()).isEqualTo("Nicolas");
+		ClientResponse response = path.get(ClientResponse.class);
+		assertThat(response.getType()).isEqualTo(MediaType.APPLICATION_XML_TYPE);
+		assertThat(response.getStatus()).isEqualTo(Status.OK.getStatusCode());
+		Hello hello = response.getEntity(Hello.class);
+		assertThat(hello).isNotNull();
+		assertThat(hello.getMessage()).isEqualTo("Hello");
+		assertThat(hello.getName()).isEqualTo("Nicolas");
 	}
 
-	private String getFullUrl(String relativeUrl, String name){
-		return getBaseURI().toString()+relativeUrl+"/"+name;
-	}	
-	
 }
